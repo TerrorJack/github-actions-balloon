f8de87edb4dcca2654384f24964397364a2968cc
Cheng Shao <cheng.shao@tweag.io>
2021-01-26 11:54:12 +0000

    WIP

--- test.js
+++ test.js
@@ -1,6 +1,11 @@
+const fs = require("fs");
 const webdriver = require("selenium-webdriver");
 
 (async () => {
-  let driver = await new webdriver.Builder().forBrowser("safari").build();
-  console.log(driver);
+  const driver = await new webdriver.Builder().forBrowser("safari").build();
+  await driver.get("https://wasm-feature-detect.surma.technology");
+  fs.writeFileSync(
+    "screenshot.png",
+    Buffer.from(await driver.takeScreenshot(), "base64")
+  );
 })();
