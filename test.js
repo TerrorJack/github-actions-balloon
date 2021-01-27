537437f858cab63522738b429fc0a3312fd8ba1d
Cheng Shao <cheng.shao@tweag.io>
2021-01-27 13:00:44 +0000

    WIP

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 const fs = require("fs");
 const { Driver, Options } = require("selenium-webdriver/safari");
+const util = require("util");
 
 (async () => {
   const options = new Options()
     .setTechnologyPreview(true)
     .setBrowserName("Safari Technology Preview");
   const driver = Driver.createSession(options);
+  console.log(await driver.getCapabilities());
   await driver.get("https://httpbin.org/anything");
   console.log(await driver.getPageSource());
   await driver.get("https://wasm-feature-detect.surma.technology");
